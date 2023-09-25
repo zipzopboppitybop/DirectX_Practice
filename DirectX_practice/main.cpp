349c4b89ce080fa9df9e5557dda7abc1de87d4af
BJ_COMPUTER\ender <bw49078@gmail.com>
2023-09-25 13:44:26 -0500

    Can create a window that doesn't do anything.

--- DirectX_practice/main.cpp
+++ DirectX_practice/main.cpp
@@ -7,6 +7,37 @@ int CALLBACK WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow   )
 {
+	// class name
+	const auto pClassName = "DirectX_Window";
+
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hCursor = nullptr;
+	wc.hIcon = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	// create window instance
+	// takes in a dwExStyle, class name, window name, style(s) (can add multiple with |), x and y for position on screen, width and height, parent menu, child menus, hinstance, LPVOID
+	HWND hWnd = CreateWindowEx(
+		0,
+		pClassName,
+		"Hello",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200,200,640,480,
+		nullptr,nullptr,hInstance,nullptr
+		);
+	// Actually show the created window
+	ShowWindow(hWnd, SW_SHOW);
 	while (true);
 	return 0;
 }
